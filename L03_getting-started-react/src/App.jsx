d7eff64f2e3676bc456c8e82fc2ce4670c4b166c
liLyxws <estrellalychee15@gmail.com>
2026-07-19 11:53:26 +0900

    Done with laboratory 3

---
+++ L03_getting-started-react/src/App.jsx
@@ -0,0 +1,48 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <section>
+      
+        <h1>ABOUT ME</h1>
+        <hr />
+        
+
+        <h2>Full Name</h2>
+        <p>Melissa Guinte Estrella</p>
+
+        <h2>Course & Year</h2>
+        <p>Bachelor of Science in Information Systems (BSIS) – 3rd Year</p>
+
+        <h2>Fun Fact</h2>
+        <ul>
+          <li>I enjoy building websites, though I am still learning and not that good yet.</li>
+        </ul>
+
+        <hr />
+
+        <h2>My Reflection</h2>
+        <p>
+          So far, what I have learned in this course is that at first, 
+          it was hard for me as a student, but I am still trying to keep 
+          up.<br></br> I also learned some things by myself. For example, I learned 
+          about Git Bash commands because our teacher teaches it well.<br></br>
+          I also learned how to do the SSH key for our GitHub. It is not 
+          just about learning as a student, but also learning as personal <br></br>
+          development, because we need to be disciplined when it comes to 
+          following the requirements and submitting on time. <br></br>This will help 
+          us be more ready for our future, especially when it comes to skills 
+          and discipline in the real world.
+        </p>
+
+        <br></br>
+        
+        <br></br>
+        
+      </section>
+    </>
+  )
+}
+
+export default App
